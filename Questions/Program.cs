56089463b775fc6da0d5cce41d97f277b2fde5fb
yxshu <yxshu@qq.com>
2017-06-06 14:19:50 +0800

    小修改
    小修改

--- Questions/Program.cs
+++ Questions/Program.cs
@@ -11,7 +11,7 @@ namespace Questions
     {
         static void Main(string[] args)
         {
-            string[] documents = new string[] { "QuestionLibraries/instruction-yuxiangshu.docx" };
+            string[] documents = new string[] { "QuestionLibraries/avoidcollision-wufei.docx" };
             //"QuestionLibraries/avoidcollision-wufei.docx"  ,
             // "QuestionLibraries/certificate-yuxiangshu.docx",
             //"QuestionLibraries/english-xiangwei.docx",
@@ -28,16 +28,16 @@ namespace Questions
             Regex regA = new Regex("[ABCDabcd]{1}[\\.|、]", RegexOptions.IgnoreCase);//A|B|C|D
             Regex regNO = new Regex("^[0-9]+[\\.|、]", RegexOptions.IgnoreCase);//以数字开头  题干
             Regex regexpstar = new Regex("^参考答案");//参考答案开头
-            Regex regexp = new Regex("^[0-9]+[\\.|、][ABCDabcd]{1}[\\.|、]");//解释
+            Regex regexp = new Regex("^[0-9]+[\\.|、][ABCDabcd]{1}[\\.|、|。]");//解释
             Regex regChapter = new Regex("^第[一二三四五六七八九十]{1,3}章", RegexOptions.IgnoreCase);//章标题
             Regex regNode = new Regex("^第[一二三四五六七八九十]{1,3}节", RegexOptions.IgnoreCase);//节标题
             Regex regxhx = new Regex("[_]{4,10}", RegexOptions.IgnoreCase);//下划线
-            //StreamWriter writer = new StreamWriter("D://error.txt", true, System.Text.Encoding.Default, 1 * 1024);
+            StreamWriter writer = new StreamWriter("D://error.txt", true, System.Text.Encoding.Default, 1 * 1024);
             foreach (string str in documents)
             {
                 string path = @"d:/" + str;
                 Console.WriteLine(path);
-                //writer.WriteLine(path);
+                writer.WriteLine(path);
                 try
                 {
                     // using (FileStream stream = File.OpenRead(path))
@@ -53,7 +53,7 @@ namespace Questions
                         ref unknow, ref unknow, ref unknow, ref unknow, ref unknow,
                         ref unknow, ref unknow, ref unknow, ref unknow, ref unknow);
                     int paragraphsCount = doc.Paragraphs.Count;
-                    for (int i = 299; i < paragraphsCount;i++ )
+                    for (int i = 299; i < paragraphsCount; i++)
                     {
                         Console.WriteLine(i);
                         Word.Range para = doc.Paragraphs[i].Range;
@@ -65,9 +65,9 @@ namespace Questions
                             expstar = false;
                             chapter = text;
                             Console.WriteLine(mark);
-                            //writer.WriteLine(mark);
+                            writer.WriteLine(mark);
                             mark = 0;
-                            //writer.WriteLine(text);
+                            writer.WriteLine(text);
                             Console.WriteLine("章标题： " + chapter);
                         }
                         else if (regNode.IsMatch(text))//节标题
@@ -75,15 +75,15 @@ namespace Questions
                             expstar = false;
                             node = text;
                             Console.WriteLine(mark);
-                            // writer.WriteLine(mark);
+                            writer.WriteLine(mark);
                             mark = 0;
-                            // writer.WriteLine(node);
+                            writer.WriteLine(node);
                             Console.WriteLine("节标题： " + node);
                         }
                         else if (regexpstar.IsMatch(text))//参考答案开始
                         {
                             expstar = true;
-                            Console.WriteLine("参考答案开始： " + text);
+                            Console.WriteLine("参考答案开始标记： " + text);
 
                         }
                         else if (regNO.IsMatch(text))//数字开头的
@@ -104,9 +104,9 @@ namespace Questions
                                 else//其它， 不知道是什么情况，有可能是判断题
                                 {
                                     mark++;
-                                    //writer.WriteLine("其他： " + regA.Split(text).Length + "_" + text);
+                                    writer.WriteLine("其他：数字开头，不是三个/四个选项- " + regA.Split(text).Length + "_" + text);
                                     Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("其他： " + regA.Split(text).Length + "_" + text);
+                                    Console.WriteLine("其他：数字开头，不是三个/四个选项- " + regA.Split(text).Length + "_" + text);
                                     Console.ReadLine();
                                 }
                             }
@@ -118,7 +118,7 @@ namespace Questions
                                 }
                                 else//错误部分
                                 {
-                                    //writer.WriteLine("错误：数字开头无下划线 - " + text);
+                                    writer.WriteLine("错误：数字开头无下划线 - " + text);
                                     Console.ForegroundColor = ConsoleColor.Red;
                                     Console.WriteLine("错误：数字开头无下划线 - " + text);
                                     Console.ReadLine();
@@ -128,9 +128,9 @@ namespace Questions
                         }
                         else
                         {
-                            //writer.WriteLine("错误： " + text);
+                            writer.WriteLine("错误：非章节标题，非数字开头，你是个什么鬼- " + text);
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("错误： " + text);
+                            Console.WriteLine("错误：非章节标题，非数字开头，你是个什么鬼- " + text);
                             Console.ReadLine();
                         }
                         Console.ResetColor();
@@ -140,14 +140,15 @@ namespace Questions
                     //}
                 }
                 catch (Exception ex)
-                { //writer.WriteLine(ex.Message);
+                {
+                    writer.WriteLine(ex.Message);
                     Console.WriteLine(ex.Message);
                     Console.ReadLine();
                 }
                 finally
                 {
-                    //writer.Flush();
-                    // writer.Close();
+                    writer.Flush();
+                    writer.Close();
                 }
             }
             Console.WriteLine("完成");

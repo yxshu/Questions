a4829df51a6358f574bbd88dbc91b60e7fe63215
yxshu <yxshu@qq.com>
2017-08-25 01:31:41 +0800

    差不多了
    差不多了

--- Questions/Program.cs
+++ Questions/Program.cs
@@ -17,8 +17,8 @@ namespace Questions
     {
         static void Main(string[] args)
         {
-            string[] documents = new string[] { "QuestionLibraries/instruction-yuxiangshu.docx", "QuestionLibraries/english-xiangwei.docx", "QuestionLibraries/avoidcollision-wufei.docx", "QuestionLibraries/management-lizhite.docx", "QuestionLibraries/equipment-hedetao.docx", "QuestionLibraries/navigation-hedetao.docx", "QuestionLibraries/ocean-hedetao.docx" };//"QuestionLibraries/certificate-yuxiangshu.docx", 
-            string[] subjects = new string[] { "船舶结构与货运", "航海英语", "船舶操纵与避碰", "船舶管理", "航海学(航海仪器)", "航海学(航海地文、天文)", "航海学(航海气象与海洋学)" };//"海船船员合格证培训",
+            string[] documents = new string[] { "QuestionLibraries/navigation-hedetao.docx","QuestionLibraries/management-lizhite.docx","QuestionLibraries/instruction-yuxiangshu.docx", "QuestionLibraries/english-xiangwei.docx", "QuestionLibraries/avoidcollision-wufei.docx",  "QuestionLibraries/equipment-hedetao.docx",  "QuestionLibraries/ocean-hedetao.docx" };//"QuestionLibraries/certificate-yuxiangshu.docx", 
+            string[] subjects = new string[] { "航海学(航海地文、天文)","船舶管理",  "船舶结构与货运", "航海英语", "船舶操纵与避碰", "航海学(航海仪器)", "航海学(航海气象与海洋学)" };//"海船船员合格证培训",
             bool expstar = false;//解析开始标记
             string subject = string.Empty;
             string chapter = string.Empty;//章标题
